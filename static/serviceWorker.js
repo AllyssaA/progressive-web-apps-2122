e0848e7640f01b6e7ff5f2687b2037472e0e7008
Allyssa <a.alimoestar@gmail.com>
2022-04-01 10:47:10 +0200

    :construction: :sparkles: added serviceworker

--- static/serviceWorker.js
+++ static/serviceWorker.js
@@ -1,11 +1,31 @@
+const OFFLINE = "offline.ejs";
 self.addEventListener("install", (event) => {
-    console.log("Installing");
-    event.waitUntil(
-        caches.open("test-cache").then((cache) => {
-            return cache.addAll([
-                "/offline",
-                "/css/style.css"
-            ])
-        })
-    )
+  console.log("Installing");
+  event.waitUntil(
+    caches.open("test-cache").then((cache) => {
+      return cache.addAll(["/css/style.css"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log(event.request.url);
+  if (event.request.mode === "navigate") {
+    event.respondWith(async () => {
+      try {
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        console.log("Fetch failed, returning offline page.", error);
+
+        const cache = await caches.open("test");
+        const cachedResponse = await cache.match(OFFLINE);
+        return cachedResponse
+      }
+    });
+  }
 });
